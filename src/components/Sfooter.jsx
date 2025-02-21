ab63811da8dbd878aa3dc9d3b93f3f232fd9e331
Achinta Haldar <107491271+FSwebdeveloper@users.noreply.github.com>
2025-02-21 21:18:19 +0530

    Update Sfooter.jsx

--- src/components/Sfooter.jsx
+++ src/components/Sfooter.jsx
@@ -27,7 +27,7 @@ function Sign() {
     event.preventDefault();
     setFormErrors(validate(formValues));
     setIsSubmit(true);
-    fetch("http://localhost:8080/",{
+    fetch("http://localhost:8080/sign",{
     method:"POST",
     body:JSON.stringify(formValues),
     headers:{
